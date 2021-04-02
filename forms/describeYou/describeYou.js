ecce9e2454a9abae16d0ed5b68db2248a018bdd4
NoahSchalley <70349070+NoahSchalley@users.noreply.github.com>
2021-04-02 15:08:05 -0500

    Commit 1

--- forms/describeYou/describeYou.js
+++ forms/describeYou/describeYou.js
@@ -1,5 +1,8 @@
+rdPersonality.onchange=function(){
+    let userChoice = $("input[name=rdPersonality]:checked").prop("value")
+   console.log(`I would agree that you are a ${userChoice} person too!`)
+}
 
-
-
-
-
+btnNewForm2.onclick=function(){
+  ChangeForm(favExercises)
+}
--- forms/favExercises/favExercises.js
+++ forms/favExercises/favExercises.js
@@ -2,9 +2,27 @@ var exercises = ["pullups","pushups","situps","jogging","plank","curls"]
 var coreExercises = ["situps","plank"]
 
 favExercises.onshow=function(){
-
+    selExercises.clear()   
+ for (i = 0; i < exercises.length; i++) 
+        selExercises.addItem(exercises[i])
 }
 
 
 
+btnDone.onclick=function(){
+let message = ""
+  for (i = 0; i < selExercises.text.length; i++)
+     message = message + selExercises.text[i] + ", "
+  if(message == "situps, plank, ")
+    console.log("You chose situps and planks - those are the two core exercises.")
+  else
+    console.log(`You did not pick the two core exercises.`)
+    }
+
+
+btnNewForm3.onclick=function(){
+  ChangeForm(mobileNav)
+}
+
+    
 
